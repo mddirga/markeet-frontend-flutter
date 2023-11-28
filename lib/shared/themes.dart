21878e88082d589433c7ef375e59ec10b02085f5
madedirga22 <madedirga22@gmail.com>
2023-11-28 11:54:35 +0800

    Project Init

---
+++ lib/shared/themes.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+double defaultMargin = 24.0;
+double defaultRadius = 17.0;
+
+Color primaryColor = const Color(0xff8E81E8);
+Color secondaryColor = const Color(0xff38ABBE);
+Color alertColor = const Color(0xffED6363);
+Color priceColor = const Color(0xff2C96F1);
+Color checkColor = const Color(0xff6C5ECF);
+Color subtitleColor = const Color(0xff504F5E);
+Color textFormColor = const Color(0xffACABB2);
+Color textFormBackgroundColor = const Color(0xffD8D8D8);
+Color backgroundColor1 = const Color(0xffF1F1F1);
+Color backgroundColor2 = const Color(0xffFFFFFF);
+Color backgroundColor3 = const Color(0xff92E3FD);
+Color backgroundColor4 = const Color(0xffDDD2D2);
+Color backgroundColor5 = const Color(0xffD2CEF4);
+Color backgroundColor6 = const Color(0xffE7B8B8);
+Color wishlistCardBackgroundColor = const Color(0xffDEDFE5);
+Color primaryTextColor = const Color(0xff2E2E2E);
+Color primaryCardColor = const Color(0xffECEDEF);
+Color secondaryTextColor = const Color(0xffF1F0F2);
+Color transparentColor = Colors.transparent;
+
+TextStyle primaryTextStyle =
+    TextStyle(fontFamily: "Open+Sans", color: primaryTextColor);
+
+TextStyle secondaryTextStyle =
+    TextStyle(fontFamily: "Open+Sans", color: secondaryTextColor);
+
+TextStyle priceTextStyle =
+    TextStyle(fontFamily: "Open+Sans", color: priceColor);
+
+TextStyle viewAllTextStyle =
+    TextStyle(fontFamily: "Open+Sans", color: alertColor);
+
+TextStyle subtitleTextStyle =
+    TextStyle(fontFamily: "Open+Sans", color: subtitleColor);
+
+TextStyle textFormTextStyle =
+    TextStyle(fontFamily: "Open+Sans", color: textFormColor);
+
+TextStyle registerTextStyle =
+    TextStyle(fontFamily: "Open+Sans", color: primaryColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
